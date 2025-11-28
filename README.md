8ed280343779d6b0821031aa085a407997536d98
jerrybaijy <jerry.baijy@outlook.com>
2025-11-28 21:11:17 +0800

    多容器集成测试完成；添加 README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todos Fullstack
+
+<img src="assets/image-20251128210905428.png" alt="image-20251128210905428" style="zoom:80%;" />
--- backend/config.py
+++ backend/config.py
@@ -6,13 +6,13 @@ load_dotenv()
 
 class Config:
     # Flask 配置
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'  
 
-    # 数据库配置
+    # 获取数据库连接信息
     DB_USER = os.environ.get('MYSQL_USER') or 'root'
-    DB_PASS = os.environ.get('MYSQL_PASSWORD') or 'password'
+    DB_PASS = os.environ.get('MYSQL_PASSWORD') or 'password'        
     DB_HOST = os.environ.get('DB_HOST') or 'localhost'
-    DB_NAME = os.environ.get('MYSQL_DATABASE') or 'todos_db'
+    DB_NAME = os.environ.get('MYSQL_DATABASE') or 'todos_db'        
 
     # SQLAlchemy 配置
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset=utf8mb4'
